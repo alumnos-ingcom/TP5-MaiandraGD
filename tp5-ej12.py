3b53ac2b5eb40a22060bdb52f279dd03beaf59de
Yuli <pinchabon@gmail.com>
2021-06-12 19:50:41 -0300

    Create tp5-ej12.py

---
+++ tp5-ej12.py
@@ -0,0 +1,28 @@
+################
+# Giuliano Daniele - @MaiandraGD
+# UNRN Andina - Introducción a la Ingeniería en Computación
+################
+
+# Esta funcion no analiza si los números se repiten o no
+
+def lista_igual(l1=0,l2=0):       
+    
+    lista1 = list(input("Ingrese números para la primer lista: "))
+    lista2 = list(input("Ingrese números para la segunda lista: "))
+    
+    #reviso si cada elemento de lista 1 está en lista 2
+    for i in lista1:        
+        if i not in lista2:
+            return False
+
+    #reviso si cada elemento de lista 2 está en lista 1
+    for i in lista2:
+        if i not in lista1:
+            return False
+    
+    return True
+        
+    
+    
+if __name__ == "__main__":
+    print(lista_igual())
