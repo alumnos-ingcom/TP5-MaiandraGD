94ccf96c20adc14e07bbe1ae5c1320e5bc9e4385
Yuli <pinchabon@gmail.com>
2021-06-13 12:54:40 -0300

    añadido prueba()
    @martinvilu yo de nuevo, jaja. A esto te referías con lo de prueba()? Así esta bien? o que falta?

--- tp5-ej12.py
+++ tp5-ej12.py
@@ -7,8 +7,10 @@
 
 def lista_igual():       
     
+    
     lista1 = list(input("Ingrese números para la primer lista: "))
     lista2 = list(input("Ingrese números para la segunda lista: "))
+    
     
     #reviso si cada elemento de lista 1 está en lista 2
     for i in lista1:        
@@ -19,10 +21,13 @@ def lista_igual():
     for i in lista2:
         if i not in lista1:
             return False
-    
+
     return True
         
-    
+        
+def prueba():
+   print(lista_igual()) 
     
 if __name__ == "__main__":
-    print(lista_igual())
+    prueba()
+    
